06cb3768ca092249eec86c96f766ef95156af15e
Adam <37848102+Blaadam@users.noreply.github.com>
2025-01-31 10:46:56 +0000

    Add project files.

---
+++ InostyProject/Data/InostyContext.cs
@@ -0,0 +1,20 @@
+﻿using InostyApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InostyApp.Data
+{
+    public class InostyContext: DbContext
+    {
+        public InostyContext(DbContextOptions<InostyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> UserTable { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Account>().To
+        }
+
+    }
+}
---
+++ InostyProject/Data/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InostyProject.Data
+{
+    public class User
+    {
+        [Key]
+        public int AccountID { get; set; }
+        [MaxLength(30)]
+        public string AccountName { get; set; }
+        [MaxLength(254)]
+        public string Email { get; set; }
+        [MaxLength(30)]
+        public string Password { get; set; }
+    }
+}
